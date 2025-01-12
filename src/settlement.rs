e38c7d60ffcd46235294d16816899225b0721694
liutian <435757679@qq.com>
2025-01-12 20:20:46 +0800

    update publish.sh

--- src/settlement.rs
+++ src/settlement.rs
@@ -1,3 +1,4 @@
+use std::convert::TryInto;
 use crate::state::State;
 use zkwasm_rest_abi::WithdrawInfo;
 pub struct SettlementInfo(Vec<WithdrawInfo>);
@@ -18,5 +19,51 @@ impl SettlementInfo {
         sinfo.0 = vec![];
         State::store();
         bytes
+    }
+}
+
+
+
+pub struct WithdrawInfo {
+    pub opinfo: u64,
+    pub account_index: u32,
+    pub object_index: u32,
+    pub amount: [u64; 4],
+    pub sender: [u8; 32],
+}
+
+pub fn encode_address(v: &Vec<u64>) -> [u8; 32] {
+    let mut bytes = Vec::with_capacity(32);
+    for i in 0..4 {
+        bytes.append(&mut v[i].to_le_bytes().to_vec());
+    }
+    bytes.try_into().unwrap()
+}
+
+impl WithdrawInfo {
+    pub fn new(
+        nounce: u64,
+        account_index: u32,
+        object_index: u32,
+        amount: [u64; 4],
+        sender: [u8; 32],
+    ) -> Self {
+        WithdrawInfo {
+            opinfo: (WITHDRAW as u64) + (nounce << 8),
+            account_index,
+            object_index,
+            amount,
+            sender,
+        }
+    }
+    /// change everything to big endian that should fits solidity's format
+    pub fn to_be_bytes(&self, bytes: &mut Vec<u8>) {
+        bytes.append(&mut self.opinfo.to_be_bytes().to_vec());
+        bytes.append(&mut self.account_index.to_be_bytes().to_vec());
+        bytes.append(&mut self.object_index.to_be_bytes().to_vec());
+        for i in 0..4 {
+            bytes.append(&mut self.amount[3 - i].to_be_bytes().to_vec());
+        }
+        bytes.append(&mut self.sender.to_vec());
     }
 }
