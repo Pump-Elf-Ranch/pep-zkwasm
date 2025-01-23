eb5d3dd14f902523401f830d920de39475a7a922
leo <liutianv1@gmail.com>
2025-01-23 19:35:47 +0800

    update

--- src/state.rs
+++ src/state.rs
@@ -641,7 +641,7 @@ impl Transaction {
 
             _ => {
                 unsafe { require(*pkey == *ADMIN_PUBKEY) };
-                zkwasm_rust_sdk::dbg!("admin tick start \n");
+                zkwasm_rust_sdk::dbg!("admin tick s \n");
                 STATE.0.borrow_mut().queue.tick();
                 0
             }
