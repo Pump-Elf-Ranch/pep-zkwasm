4eaf30bf8f737d7617586dca2abcea5cc1462cac
liutian <435757679@qq.com>
2025-01-23 23:26:56 +0800

    fix

--- src/state.rs
+++ src/state.rs
@@ -641,7 +641,7 @@ impl Transaction {
 
             _ => {
                 self.check_admin(pkey).map_or_else(|e| e, |_| 0);
-                zkwasm_rust_sdk::dbg!("admin tick f \n");
+                zkwasm_rust_sdk::dbg!("admin tick g \n");
                 STATE.0.borrow_mut().queue.tick();
                 0
             }
